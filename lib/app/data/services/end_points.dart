1aafd483f9830f59af42f5c300cb9cb6e89d39e1
AhmedZaki <ahmed_zaki21@yahoo.com>
2024-09-09 19:51:55 +0300

    updated the endpoints of the apis

--- lib/app/data/services/end_points.dart
+++ lib/app/data/services/end_points.dart
@@ -1,7 +1,7 @@
 class EndPoints {
-  static const login = 'login';
-  static const postAssociateUser = 'user/add-meter';
-  static const getAssociateUser = 'IoT-devices/associated-devices';
-  static const changePowerStatus = 'user/change-power';
+  static const login = 'backend/login';
+  static const postAssociateUser = 'user/backend/add-meter';
+  static const getAssociateUser = 'IoT-devices/backend/associated-devices';
+  static const changePowerStatus = 'user/backend/change-power';
   static const getAllDevices = 'user/get-all-data';
 }
